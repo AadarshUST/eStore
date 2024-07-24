82c10a2ebab012564829504d0a4c3842baf58346
AadarshUST <aadarsh.chaurasia@ust.com>
2024-07-24 10:15:48 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import CatNav from './Components/CatNav';
+import MainContainer from './Components/MainContainer';
+import TopNav from './Components/TopNav'; /** if we name the file as index.js and the component name is same as 
+                                           the folder name then we don't have to write TopNAv two times*/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <TopNav/>
+     <CatNav/>
+     <MainContainer/>
     </div>
   );
 }
